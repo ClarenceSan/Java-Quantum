f9adb7a9c1d8e5ccf8f715d6dfed3c3e2ef8d81f
It's like a quantum logic gate, always universal <160012351+ClarenceSan@users.noreply.github.com>
2024-02-16 11:53:54 +0000

    Update one.java
    Signed-off-by: "It's like a quantum logic gate, always universal." <160012351+ClarenceSan@users.noreply.github.com>

--- java/one.java
+++ java/one.java
@@ -1,1 +1,15 @@
+import com.github.ojplg.skirho.strange.*;
+
+public class Main {
+    public static void main(String[] args) {
+        final QuantumExecutionEnvironment qee = new SimpleQuantumExecutionEnvironment();
+        final Program program = new Program(2);
+        final Step step = new Step();
+        step.addGate(new Hadamard(0));
+        step.addGate(new Cnot(0, 1));
+        program.addStep(step);
+        final Result result = qee.runProgram(program);
+        System.out.println(result);
+    }
+}
 
